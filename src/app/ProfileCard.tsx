7ca8b5112cb30a4f80ccb60157913dc0cb92485f
Ricardo Martinez <ricardo@mastercamping.com>
2024-05-19 16:35:19 +0200

    First commit for testing publish.yml nextjs to github pages

---
+++ src/app/ProfileCard.tsx
@@ -0,0 +1,47 @@
+'use client'
+
+import {FaHeart} from 'react-icons/fa';
+import {useEffect, useState} from "react";
+
+export default function ProfileCard() {
+
+    const phrases = [
+        "I ❤️ coding",
+        "I ❤️ learning new technologies",
+        "I ❤️ solving problems",
+        "I ❤️ building things",
+        "I ❤️ teaching",
+        "I ❤️ sharing knowledge",
+    ];
+
+    const [currentPhrase, setCurrentPhrase] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentPhrase((prevPhrase) => (prevPhrase + 1) % phrases.length);
+        }, 3000); // Cambiar la frase cada 3 segundos
+
+        return () => clearInterval(interval);
+    }, []);
+
+
+    return (
+        <div className="flex flex-col items-center justify-center p-6 rounded-lg max-w-sm mx-auto">
+            <div className="mb-4">
+                <img
+                    src="/images/ricardo_martinez_montes_2024.jpg"
+                    alt="Ricardo Martinez Montes"
+                    className="w-64 h-64 rounded-full mx-auto"
+                />
+            </div>
+            <div className="text-center">
+                <h1 className="text-2xl font-semibold text-gray-900">Ricardo Martinez Montes</h1>
+                <p className="text-gray-700">
+                    {phrases[currentPhrase].split('❤️')[0]}
+                    <FaHeart className="inline text-red-500 mx-1"/>
+                    {phrases[currentPhrase].split('❤️')[1]}
+                </p>
+            </div>
+        </div>
+    );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import ProfileCard from "@/app/ProfileCard";
+
+export default function Home() {
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gray-100">
+            <ProfileCard/>
+        </div>
+    );
+}
