46d99b9ee7ec3e12356eadc508186b1f086c6d54
Ricardo Martinez <mrtnz.rcrd@gmail.com>
2024-05-20 09:00:27 +0200

    All sources to public folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,10 +19,10 @@ export default function RootLayout({
     <head>
       <meta charSet="utf-8"/>
       <meta name="viewport" content="width=device-width, initial-scale=1"/>
-      <link rel="icon" href="/favicon.ico"/>
-      <link rel="apple-touch-icon" sizes="180x180" href="/public/images/apple-touch-icon.png"/>
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+      <link rel="icon" href="/images/favicon.ico"/>
+      <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png"/>
+      <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png"/>
+      <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png"/>
       <link rel="manifest" href="/site.webmanifest"/>
     </head>
     <body className={inter.className}>{children}</body>
