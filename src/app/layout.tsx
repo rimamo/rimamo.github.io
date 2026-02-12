bdafa92925db8efffa6189805231d93032fee0c8
rimamo <hola@ricardomartinezmontes.com>
2026-02-12 01:56:12 +0100

    feat: add root layout with metadata, favicons, and Google Analytics.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,7 +26,24 @@ export default function RootLayout({
       <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png"/>
       <link rel="manifest" href="/site.webmanifest"/>
     </head>
-    <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-94GFMMXZB8`}
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+        >
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-94GFMMXZB8');
+          `}
+        </Script>
+      </body>
     </html>
   );
 }
